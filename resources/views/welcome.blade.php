6329452bf960c43ea6e68453017e56b5021ba9ba
abdullarayyan <arayyan.abd@gmail.com>
2022-05-28 03:34:38 +0300

    fff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,41 +9,136 @@
 @section('content')
 
     <div class="row">
+
         <div class="col-md-12">
-            <div class="row" style="padding: 30px;border: 7px solid #dbebe8;
-    border-radius: 30px!important;">
+            <div class="row" style="
+                        padding: 30px;
+                        border-radius: 30px!important;
+                        background-color: #057a65;
+                        ">
 
 
+                <div class="col-md-4 text-center" style="
+                 border-radius:
+                 30px!important;
+                 background-color: #057a65;
+                 ">
 
-                <div class="col-md-6 text-center" style="    ">
-
-                    <p style="padding: 20%;font-weight: 700;font-size: 25px" >نبذه عن المشروع</p>
+                    <p style="
+                        padding: 5%;
+                        margin-top: 100px;
+                    font-weight: 700;
+                    font-size: 39px;
+                    color: #e4b717;
+                    ">نبذه عن المشروع</p>
                 </div>
-                <div class="col-md-6 text-center" style="">
+                <div class="col-md-8 text-center" style="
+                                 border-radius:
+                                 30px!important;
+                                 background-color: #e4b717;
+                                 ">
 
 
-                    <p style="padding: 20%">
-                        دريم هاوس هي شركة تتميز بالتنوع في التصميمات الحديثة لمنزل الأحلام وتحتوي على محترفين بجودة عالية يساعدون العملاء في بناء منازلهم أو استثماراتهم العقارية بأفضل الأسعار والمواصفات. تسعى الشركة لإرضاء عملائها وتقديم أفضل الخدمات والعروض لهم
+                    <p style="
+                            padding: 10%;
+                            width: 100%;
+                            color: #070707;
+                            font-size: x-large;
+                            font-weight: 500;
+                                    ">
+                        دريم هاوس هي شركة تتميز بالتنوع في التصميمات الحديثة لمنزل الأحلام وتحتوي على محترفين بجودة
+                        عالية يساعدون العملاء في بناء منازلهم أو استثماراتهم العقارية بأفضل الأسعار والمواصفات. تسعى
+                        الشركة لإرضاء عملائها وتقديم أفضل الخدمات والعروض لهم
 
                     </p>
 
                 </div>
+
+
             </div>
 
-{{--            <div class="row">--}}
 
 
 
-{{--                <div class="col-md-6 text-center">--}}
 
-{{--                    <p class="m-30">نبذه عن المشروع</p>--}}
-{{--                </div>--}}
-{{--                <div class="col-md-6">--}}
+            <div class="row m-t-10" style="    padding: 30px;
+    margin-top: 18px;
+    border-radius: 30px!important;
+    background-color: #057a65;
+                        ">
 
 
 
-{{--                </div>--}}
-{{--            </div>--}}
+                <div class="col-md-8 text-center" style="
+                                 border-radius:
+                                 30px!important;
+                                 background-color: #e4b717;
+                                 ">
+
+
+                    <ul style="    margin: 76px!important;">
+                        <li style="
+                        margin: 20px;
+                            width: 100%;
+                            color: #070707;
+                            font-size: x-large;
+                            font-weight: 500;">
+                            الرضا التام للعملاء والجهات المعنيه
+                        </li>
+                        <li style="
+                                                margin: 20px;
+
+                            width: 100%;
+                            color: #070707;
+                            font-size: x-large;
+                            font-weight: 500;">
+                             التنفيذ واداء الاعمال طبقا للمعاير المطلوبه
+
+                        </li>
+                        <li style="
+                                                margin: 20px;
+
+                            width: 100%;
+                            color: #070707;
+                            font-size: x-large;
+                            font-weight: 500;">
+                            المحافظه علي سمعتنا في اداء المشروع
+                        </li>
+                        <li style="
+                                                margin: 20px;
+
+                            width: 100%;
+                            color: #070707;
+                            font-size: x-large;
+                            font-weight: 500;">
+                            الحفاظ علي سلامه وصحه العاملين
+
+                        </li>
+
+                    </ul>
+
+
+                </div>
+
+
+                <div class="col-md-4 text-center" style="
+                 border-radius:
+                 30px!important;
+                 background-color: #057a65;
+                 ">
+
+                    <p style="
+                        padding: 5%;
+                        margin-top: 100px;
+                    font-weight: 700;
+                    font-size: 39px;
+                    color: #e4b717;
+                    ">اهدافنا</p>
+                </div>
+
+
+            </div>
+
         </div>
     </div>
 @endsection
